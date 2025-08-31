cd0f6b6dcfa8e260910d84d45ca1eb2a494c2a74
AdityaKumar80743 <adityakumar80743@gmail.com>
2025-08-31 08:39:04 +0530

    Added input_output.cpp (Day 1)
    Basic program to take user input and display output in C++.

---
+++ Day1-Basics/input_output.cpp
@@ -0,0 +1,12 @@
+// program to take a integer as input from the user 
+// and print it on the screen
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int input;
+    cout << "Enter a number: ";
+    cin >> input; // taking input from the user and storing it in a variable
+    cout << "you entered " << input << endl; // printing on the screen
+}
