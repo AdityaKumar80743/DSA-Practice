690db7ac35336c4d3a1bd27ef2ff627d3c681ba9
AdityaKumar80743 <adityakumar80743@gmail.com>
2025-08-31 08:45:08 +0530

    Added loops.cpp
    program to print  "Name" n times

---
+++ Day1-Basics/loops.cpp
@@ -0,0 +1,18 @@
+// program to under stand loops
+// program to print name n times
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // taking stopting limit from the user
+    int input;
+    cout << "Times to print Name: ";
+    cin >> input;
+    
+    // usign loops to print "Name" n times
+    for (int i=0; i<input; i++) {
+        cout << "Name" << endl;
+    }
+    
+}
