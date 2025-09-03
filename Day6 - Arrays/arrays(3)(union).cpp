4977400145c4b6a575568e056b46778ea2e0af50
AdityaKumar80743 <adityakumar80743@gmail.com>
2025-09-04 00:55:02 +0530

    Create arrays(3)(union).cpp
    file includes 
    - linear search
    - union of two arrays - different approach

---
+++ Day6 - Arrays/arrays(3)(union).cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+// print set till n
+void printS(set<int> &mset) {
+    for (auto it:mset) cout << it << " ";
+    cout << endl;
+}
+
+// print vector till n
+void print(vector<int> &arr, int n) {
+    for (auto i=0; i<n; i++) cout << arr[i] << " ";
+    cout << endl;
+}
+
+
+// ------------ LINEAR SEARCH ----------------
+// find the index where the target number appears first
+// iterate through the whole ray in linear manner (one by one till the end is reached)
+int linearSearch(int arr[], int n, int target) {
+    for (int i=0; i<n; i++) {
+        if (arr[i] == target) return i; // target found 
+    }
+    return -1; // target not found
+}
+// TC - O(n)
+
+
+
+// ------------- UNION OF TWO ARRAYS ----------------
+// problem: two array which are sorted, can have duplicates 
+// find union to the two array (union do not have duplicates and it is should be sorted)
+
+// using set - as set store only unique elements
+void unionArr(int arr1[], int arr2[], int n1, int n2) {
+    set<int> distinct;
+
+    for (int i=0; i<n1; i++) {
+        distinct.insert(arr1[i]);
+    } 
+    for (int i=0; i<n2; i++) {
+        distinct.insert(arr2[i]);
+    }
+    printS(distinct);
+}
+// TC - O(n1 + n2 + nlogn(insertion)) 
+// SC - O(n1 + n2)
+
+
+// using map
+void unionArr2(vector<int> arr1, vector<int> arr2) {
+    map<int, int> mpp;
+    
+    for (int i=0; i<arr1.size(); i++) {
+        mpp[arr1[i]]++; // logn
+    }
+    for (int i=0; i<arr2.size(); i++) {
+        mpp[arr2[i]]++; // logn
+    }
+    printM(mpp);
+} 
+// TC - O(logn + logn) 
+
+
+// --- optimal way 
+// using pointers
+
+// STEPS:
+// take two pointer for both arrays and create a result vector 
+// pick smallest amoung the arrays
+// if not equal to the last elem of result vector - add to result vector
+// when one of the arrays gets exhausted add the elements of other array
+// - alwasy check result vector last element before adding
+
+// TC - O(n1 + n2) 
+// Sc - O(N1+ N2)
+void unionArr3(vector<int> a1, vector<int> a2) {
+    vector<int> uniArr;
+    int n1 = a1.size();
+    int n2 = a2.size();
+    int i = 0;
+    int j = 0;
+    
+    while (i<n1 && j<n2) { // until pointer are smaller then the arrays size
+        if (a1[i] <= a2[j]) { // pick smallest among a1[i] and a2[j]
+            if (uniArr.size() == 0 || uniArr.back() != a1[i]) { // if last elem in uniArr not equal to smallest then puch smallest else move pointer
+                uniArr.push_back(a1[i]);
+            } i++;
+        }
+        else (a1[i] <= a2[j]) {
+            if (uniArr.size() == 0 || uniArr.back() != a2[j]) {
+                uniArr.push_back(a2[j]);
+            } j++;
+        }
+    }
+    
+    // IF one of the array is exhausted 
+    while (j<n2) {
+        if (uniArr.size() == 0 || uniArr.back() != a2[j]) {
+                uniArr.push_back(a2[j]);
+            } j++;
+    }
+    
+    while (i<n1) {
+        if (uniArr.size() == 0 || uniArr.back() != a1[i]) {
+                uniArr.push_back(a1[i]);
+            } i++;
+    }
+    
+    print(uniArr, uniArr.size());
+}
+
+
+int main() {
+    int arr1[5] = {1,2,3,4,5};
+    int arr2[3] = {1,2,6};
+
+    uni2(arr1, arr2, 5,3);
+
+
+    return 0;
+}
